e2b20424a87b2dd7e1f58f7769a005ac56be8ad8
Jesse Stoler <j.sto@Jesses-MacBook-Pro.local>
2018-06-16 20:56:21 -0600

    personal page

---
+++ index.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    var config = {
+        apiKey: "AIzaSyCADxm9fAAZrmo6RhUmbWgN3CY-Wn4RRJA",
+        authDomain: "personal-9ea75.firebaseapp.com",
+        databaseURL: "https://personal-9ea75.firebaseio.com",
+        projectId: "personal-9ea75",
+        storageBucket: "",
+        messagingSenderId: "557892842323"
+      };
+      firebase.initializeApp(config);
+
+      var database = firebase.database();
+      var saved = database.ref("Saved");
+      var db = saved.push();
+    
+      function contact() {
+            
+            db.set({
+                name: $("#name").val(), 
+                email: $("#email").val(),
+                message: $("#message").val(),
+                time: Date.now()
+              });
+            
+            }
+
+    });
