84f010d271d3fe45a78ab93ab49c0219e55c8fbe
mac <mac@macs-MacBook-Pro.local>
2019-05-08 20:01:25 -0600

    file update

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
-
+    var i = 0;
+    var text = "Jesse Stoler";
+    var speed = 250;
     var config = {
         apiKey: "AIzaSyCADxm9fAAZrmo6RhUmbWgN3CY-Wn4RRJA",
         authDomain: "personal-9ea75.firebaseapp.com",
@@ -14,22 +16,93 @@ $(document).ready(function(){
       var saved = database.ref("Saved");
       var db = saved.push();
 
-      $("#submit").on("click", contact);
-    
-      function contact() {
+    $("#about").on("click", bio);
+    $("#bioClose").on("click", closeBio);
+    $("#blog").on("click", blog);
+    $("#blogClose").on("click", closeBlog);
+    $("#contact").on("click", contact);
+    $("#contactClose").on("click", closeContact);
+    $("#contactButton").on("click", sendMessage);
+    $("#portfolio").on("click", portfolio);
+    $("#portfolioClose").on("click", closePortfolio);
+    setTimeout(function() { typeWriter(); }, 2975);
+    setTimeout(function() { options(); }, 6975);
+    $("#mainMenu").fadeIn(5000);
 
-        if ($("#name").val() == "" || $("#email").val() == "" || $("#message").val() == "") {
+    function closePortfolio() {
+        $("#mainMenu").css("display", "block");
+        $("#highlights").css("display", "none");
+    }
+
+    function portfolio() {
+        $("#mainMenu").css("display", "none");
+        $("#highlights").css("display", "block");
+        
+    }
+
+    function contact() {
+        $("#contactForm").css("display", "block");
+        $("#person").val("");
+        $("#email").val("");
+        $("#message").val("");
+       
+
+    }
+
+    function closeContact() {
+        $("#contactForm").css("display", "none");
+    }
+ 
+    function closeBio() {
+        $("#bio").css("display", "none");
+    }
+
+    function closeBlog() {
+        $("#blogIntro").css("display", "none");
+    }
+
+    function typeWriter() {
+       
+        if (i < text.length) {
+            document.getElementById("jesse").innerHTML += text.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+        }
+    }
+
+    function options() {
+        $("h4").css("visibility", "visible");
+    }
+
+    function bio() {
+        $("#bio").css("display", "block");
+    }
+
+    function blog() {
+        $("#blogIntro").css("display", "block");
+    }
+
+   
+
+
+ 
+
+    
+      function sendMessage() {
+
+        if ($("#person").val() == "" || $("#email").val() == "" || $("#message").val() == "") {
             alert("Please fill in every field to continue");
         }
         else {
             
             alert("Message received. Thank you!")
             db.set({
-                name: $("#name").val(), 
+                name: $("#person").val(), 
                 email: $("#email").val(),
                 message: $("#message").val()
                 
               });
+              closeContact();
             }
             
             }
